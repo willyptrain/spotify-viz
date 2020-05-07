342c63eeab577a456524ce2dbefdc30aa7f88349
Nick <figginsn@gmail.com>
2020-05-07 17:41:50 -0400

    Changed color scheme to allow for more colors

--- music.py
+++ music.py
@@ -8,10 +8,9 @@ import math
 import random
 
 
-token = spotipy.util.prompt_for_user_token('fignick', 'streaming', client_id='eb173f7af2f14a189be9011019c90df2', client_secret='859a9fd7c02d42dbaa60519163071386', redirect_uri='http://localhost')
 
 
-sp = spotipy.Spotify(auth=token)
+sp = spotipy.Spotify(client_credentials_manager=SpotifyCredentials())
 
 class Track:
 
