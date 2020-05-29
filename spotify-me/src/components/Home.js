64531a4b9bd499f640a3fb86bbbca3e5ef348015
Nick Figgins <figginsn@gmail.com>
2020-05-29 13:41:29 -0400

    deleted something

--- spotify-me/src/components/Home.js
+++ spotify-me/src/components/Home.js
@@ -25,13 +25,6 @@ class Home extends React.Component{
             <div>
             <NavBar userInfo={this.props.userInfo} logout={this.logout}/>
             <h1>Hello!
-            <SpotifyApiContext.Provider value={'AQC2UbyKn-I4gAVOXhz7oy2f-zNzCCBLNjS0PU9c3O_2McnetzOP__aIdN_ZR5P7nAp-gpJLSkpeWuQBYmJdeZgRAPey7fmgBZH3Mpne561Fwe77JrGNG0UllMYq9oHFPPnKAyp4nlGTcv6-CY1PZl2v2U6ccO9-ar7_s7wxojRYQ2M9svOQOzo4ti3UpVflKvKPk_i9CISc_bBu'}>
-            <Album id={`0i3evFc1mhTVNHaF0DFwog`}>
-            {({ data }) => {
-                return <>{data}</>;
-            }}
-            </Album>
-            </SpotifyApiContext.Provider>
             </h1>
             </div>
         );
