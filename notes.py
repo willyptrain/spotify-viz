b740ef4213263e70d90da0b644ce47ce59a66727
Nick Figgins <figginsn@gmail.com>
2020-06-29 11:23:31 -0400

    hello

--- notes.py
+++ notes.py
@@ -9,6 +9,7 @@ sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 
 
+
 #nodes = artist's tracks, neighborhood=artist or genre (genre could also be class label to be predicted), edges=features of the songs, maybe artist -> neighborhood compression
 #center node = artist (surrounding nodes are directed toward the artist)
 #or maybe neighborhood is given a label, which corresponds to genre of the neighborhood's artist
