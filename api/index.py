b1dbfdc95737134aa4afdffb00f3ce6766396eba
Nick Figgins <figginsn@gmail.com>
2020-08-28 20:22:17 -0400

    updated build

--- api/index.py
+++ api/index.py
@@ -58,10 +58,6 @@ def create_app():
     def favorites():
         return app.send_static_file('index.html')
     
-    @app.route('/track/<uri>', methods=["GET"])
-    def track(uri):
-        return app.send_static_file('index.html')
-    
     @app.route('/recommended', methods=["GET"])
     def recommendations():
         return app.send_static_file('index.html')
