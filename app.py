46fea66294ca55924e5217382bea06cf24a101b2
Nick Figgins <figginsn@gmail.com>
2020-09-06 12:27:31 -0400

    reverting back to port 80

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 443))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
     #app.run()
 
 
